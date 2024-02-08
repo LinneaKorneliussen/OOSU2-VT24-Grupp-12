1bfed00653ce8fae0e9f2a125753f7877a4c1290
LinneaKorneliussen <153073744+LinneaKorneliussen@users.noreply.github.com>
2024-02-08 21:29:32 +0100

    Fel med datumen

--- PatientBL/AppointmentController.cs
+++ PatientBL/AppointmentController.cs
@@ -41,15 +41,18 @@ namespace PatientBL
         }
         public List<Appointment> GetAppointmentsForDateTime(DateTime appointmentDateTime)
         {
-            return unitOfWork.AppointmentRepository.Find(a => (a.DateAndTime >= appointmentDateTime && a.DateAndTime < a.EndDateTime) ||
-            (a.EndDateTime > appointmentDateTime && a.DateAndTime < appointmentDateTime)).ToList();
+            List<Appointment> appointments = unitOfWork.AppointmentRepository.Find(a => (a.DateAndTime <= appointmentDateTime && appointmentDateTime < a.EndDateTime) || 
+            (a.DateAndTime < appointmentDateTime && a.EndDateTime >= appointmentDateTime)).ToList();
+
+            return appointments;
         }
         public List<Staff> GetAllAvailableDoctors(DateTime appointmentDateTime)
         {
             List<Staff> allDoctors = GetAllDoctors();
             List<Appointment> appointments = GetAppointmentsForDateTime(appointmentDateTime);
 
-            List<Staff> availableDoctors = allDoctors.Where(doctor => !appointments.Any(appointment => appointment.Doctor.StaffId == doctor.StaffId)).ToList();
+            List<Staff> availableDoctors = allDoctors.Where(doctor =>
+                !appointments.Any(appointment => appointment.Doctor.StaffId == doctor.StaffId)).ToList();
 
             return availableDoctors;
         }
