5f5089cd771278a45ea8687e6bfbe7957544ff06
BeataJacobsson <153110166+S2205112@users.noreply.github.com>
2024-02-09 11:47:05 +0100

    Diagnosis form

--- PatientMSWinForms/RegisterDiagnosisViewForm.cs
+++ PatientMSWinForms/RegisterDiagnosisViewForm.cs
@@ -14,14 +14,41 @@ namespace PatientMSWinForms
 {
     public partial class RegisterDiagnosisViewForm : Form
     {
+        private DiagnosisController diagnosisController;
+        private PatientController patientController;    
+        private Patient patient; 
         public RegisterDiagnosisViewForm()
         {   
             InitializeComponent();
+            patientController = new PatientController();    
+            diagnosisController = new DiagnosisController();
         }
 
-
+        #region Register new diagnosis clicks
         private void btnFind_Click(object sender, EventArgs e)
         {
+            string ssn = txtSSN.Text;
+            patient = patientController.GetPatient(ssn);
+            if (patient != null)
+            {
+                lbl_DisplayPInfo.Text = $"Patient found: {patient.Name} with personal number {ssn}";
+            }
+            else
+            {
+                MessageBox.Show($"Patient with personal number {ssn} not found.\nTry again!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                lbl_DisplayPInfo.Text = $"Patient with personal number {ssn} not found.";
+            }
+
+        }
+
+        private void btnAddDiagnosis_Click(object sender, EventArgs e)
+        {
+            string description = txtDescription.Text;   
+            string treatment = txtTreatmentplan.Text;
+            
+            diagnosisController.AddDiagnosis(patient, description, treatment);
+            MessageBox.Show($"Diagnosis registerd successfully for {patient.Name}\nMain diagnosis: {description}\nExpected treatment: {treatment}");
+            this.Close();
 
         }
 
@@ -29,10 +56,7 @@ namespace PatientMSWinForms
         {
             Close();
         }
+        #endregion
 
-        private void btnAddDiagnosis_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
