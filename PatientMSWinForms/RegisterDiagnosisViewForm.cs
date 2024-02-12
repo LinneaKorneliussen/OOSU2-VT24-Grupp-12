02a8007be73ed345456eceaaef8a2165670218aa
Celinalinnerblom <153074108+Celinalinnerblom@users.noreply.github.com>
2024-02-12 10:55:38 +0100

    updaterad diagnos

--- PatientMSWinForms/RegisterDiagnosisViewForm.cs
+++ PatientMSWinForms/RegisterDiagnosisViewForm.cs
@@ -14,13 +14,15 @@ namespace PatientMSWinForms
 {
     public partial class RegisterDiagnosisViewForm : Form
     {
-        private DiagnosisController diagnosisController;
-        private PatientController patientController;    
-        private Patient patient; 
+       
+    private DiagnosisController diagnosisController;
+        private PatientController patientController;
+        private Patient patient;
+
         public RegisterDiagnosisViewForm()
-        {   
+        {
             InitializeComponent();
-            patientController = new PatientController();    
+            patientController = new PatientController();
             diagnosisController = new DiagnosisController();
         }
 
@@ -28,6 +30,12 @@ namespace PatientMSWinForms
         private void btnFind_Click(object sender, EventArgs e)
         {
             string ssn = txtSSN.Text;
+            if (string.IsNullOrEmpty(ssn))
+            {
+                MessageBox.Show("Please enter the patient's personal number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             patient = patientController.GetPatient(ssn);
             if (patient != null)
             {
@@ -38,18 +46,30 @@ namespace PatientMSWinForms
                 MessageBox.Show($"Patient with personal number {ssn} not found.\nTry again!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 lbl_DisplayPInfo.Text = $"Patient with personal number {ssn} not found.";
             }
-
         }
 
         private void btnAddDiagnosis_Click(object sender, EventArgs e)
         {
-            string description = txtDescription.Text;   
-            string treatment = txtTreatmentplan.Text;
-            
-            diagnosisController.AddDiagnosis(patient, description, treatment);
-            MessageBox.Show($"Diagnosis registerd successfully for {patient.Name}\nMain diagnosis: {description}\nExpected treatment: {treatment}");
-            this.Close();
+            // Kontrollera om personnumret har fyllts i
+            if (patient == null)
+            {
+                MessageBox.Show("Please find the patient before adding a diagnosis.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
+            string description = txtDescription.Text;
+            string treatment = txtTreatmentplan.Text;
+
+            // Kontrollera om behandlingsplanen har fyllts i
+            if (string.IsNullOrEmpty(treatment) || string.IsNullOrEmpty(description))
+            {
+                MessageBox.Show("Please enter the treatment plan before adding a diagnosis.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            diagnosisController.AddDiagnosis(patient, description, treatment);
+            MessageBox.Show($"Diagnosis registered successfully for {patient.Name}\nMain diagnosis: {description}\nExpected treatment: {treatment}");
+            this.Close();
         }
 
         private void btnClose_Click(object sender, EventArgs e)
