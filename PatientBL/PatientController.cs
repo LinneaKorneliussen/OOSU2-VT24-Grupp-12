f76431a9eb3028b147f577bb8058035190275aec
BeataJacobsson <153110166+S2205112@users.noreply.github.com>
2024-02-14 15:29:05 +0100

    Patientrepository

--- PatientBL/PatientController.cs
+++ PatientBL/PatientController.cs
@@ -10,60 +10,30 @@ namespace PatientBL
 {
     public class PatientController
     {
-        private UnitOfWork unitOfWork;
-
+        private PatientRepository patientRepository;
         public PatientController()
         {
-            unitOfWork = UnitOfWork.GetInstance();
+            patientRepository = new PatientRepository();
         }
 
         #region Get patient Method
         public Patient GetPatient(string patientPersonalNumber)
         {
-            return unitOfWork.PatientRepository.FirstOrDefault(p => p.PersonalNumber == patientPersonalNumber);
+            return patientRepository.GetPatient(patientPersonalNumber);
         }
         #endregion
 
         #region Register new patient Method
         public void CreateNewPatient(string name, string personalNumber, string address, string phoneNumber, string emailAddress)
         {
-            Patient patient = new Patient(name, personalNumber, address, phoneNumber, emailAddress);
-            unitOfWork.PatientRepository.Add(patient);
-            unitOfWork.Save();
+            patientRepository.CreateNewPatient(name, personalNumber, address, phoneNumber, emailAddress);   
         }
         #endregion
 
         #region Update patient information Method
         public void UpdatePatientInfo(Patient patientToUpdate, int choice, string newValue)
         {
-            if (patientToUpdate != null)
-            {
-                switch (choice)
-                {
-                    case 0:
-                        patientToUpdate.Name = newValue;
-                        unitOfWork.Save();
-                        break;
-                    case 1:
-                        patientToUpdate.Address = newValue;
-                        unitOfWork.Save();
-                        break;
-                    case 2:
-                        patientToUpdate.Phonenumber = newValue;
-                        unitOfWork.Save();
-                        break;
-                    case 3:
-                        patientToUpdate.EmailAddress = newValue;
-                        unitOfWork.Save();
-                        break;
-
-                    default:
-                    break;
-                
-                }
-
-            }
-         
+            patientRepository.UpdatePatientInfo(patientToUpdate, choice, newValue); 
         }
 
         #endregion
